e7828944c75cc549a695e0d8646ee078694accbd
jsonzjp <jiazjp@163.com>
2018-06-11 14:01:44 +0800

    commit code

--- app/src/main/java/com/zjp/keyboard/Case1Activity.java
+++ app/src/main/java/com/zjp/keyboard/Case1Activity.java
@@ -1,18 +1,47 @@
 package com.zjp.keyboard;
 
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.zjp.keyboard.databinding.ActivityCase1Binding;
 
 /**
  * Created by zjp on 2018/6/1 17:36.
  */
 
 public class Case1Activity extends AppCompatActivity {
+
+    private ActivityCase1Binding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_case_1);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_case_1);
+
+        binding.fullscreenContent.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cuc_ieschool));
+        binding.fullscreenContent.start();
+
+        show();
+        hide();
+    }
+
+    private void hide() {
+
+        binding.fullscreenContent.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void show() {
+        binding.fullscreenContent.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
 }
--- app/src/main/java/com/zjp/keyboard/MainActivity.java
+++ app/src/main/java/com/zjp/keyboard/MainActivity.java
@@ -1,13 +1,30 @@
 package com.zjp.keyboard;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zjp.keyboard.databinding.ActivityMainBinding;
+import com.zjp.keyboard.model.Case1Model;
+import com.zjp.keyboard.model.Case2Model;
+import com.zjp.keyboard.model.TitleModel;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding bind = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        TitleModel titleModel = new TitleModel();
+        titleModel.setTitle("解决横屏情况下，软键盘弹出导致整体布局上移问题");
+
+        bind.setTitle(titleModel);
+
+        bind.setCase1(new Case1Model());
+        bind.setCase2(new Case2Model());
+
     }
 }
